9d9c8c2de53081a5fbfcf78f523bdf51b4fad9f1
Jean_M_V <84682755+clavijas@users.noreply.github.com>
2024-11-03 23:46:02 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def readme() -> str:
 
 
 setuptools.setup(
-    name="streamlit-drawable-canvas",
+    name="streamlit-drawable-canvas-rect", #added line
     version="0.9.3",
     author="Fanilo ANDRIANASOLO",
     author_email="contact@andfanilo.com",
